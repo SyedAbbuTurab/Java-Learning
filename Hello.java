2a35ff36a832381d6c047f9237da56f82becd5d6
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-07 19:23:41 -0400

    another scanner user lib

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello World!");
         
         int a = 5;
@@ -9,5 +12,12 @@ public class Hello {
 
         int product = a * b;
         System.out.println("Product: " + product);
+
+        System.out.println("Press Enter Your Name...");
+
+        String name = scanner.nextLine(); // Wait for user input before closing
+
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
     }
 }
