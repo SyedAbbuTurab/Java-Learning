9b18c0797fda1465723457ea0a783a633be11b4d
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-06 22:53:06 -0400

    basic programm\

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
     }
 }
