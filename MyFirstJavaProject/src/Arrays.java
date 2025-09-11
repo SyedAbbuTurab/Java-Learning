691f0b84a2427c348c52e743fa0a39453e58cc8e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-10 21:45:15 -0400

    packages learned

---
+++ MyFirstJavaProject/src/Arrays.java
@@ -0,0 +1,9 @@
+import test.test;
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test.sysout();
+	}
+
+}
---
+++ MyFirstJavaProject/src/test/test.java
@@ -0,0 +1,8 @@
+package test;
+
+public class test {
+	public static void sysout() {
+		// TODO Auto-generated method stub
+		System.out.println("Package called!");
+	}
+}
