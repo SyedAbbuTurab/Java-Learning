c1dab127f03eb4f755abf2323aef4472933bd25d
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-10 21:36:10 -0400

    roll dice game

--- MyFirstJavaProject/src/HelloW.java
+++ MyFirstJavaProject/src/HelloW.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class HelloW {
 	public static void main(String[] args) {
-		System.out.println("Ha");
+		System.out.println("Let's roll die!");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		Random random = new Random();
+		
+		int total = 0;
+		
+		System.out.println("Enter # of dice to roll");
+		int numOfDice = scanner.nextInt();
+		
+		if(numOfDice > 0) {
+			for (int i = 0; i < numOfDice; i++) {
+				int roll = random.nextInt(1, 7);
+				System.out.println("You rolled " + roll);
+				total+=roll;
+			}
+		} else {
+			System.out.println("Number of dice cannot be Negative or Zero");
+		}
+		
+		System.out.println("Your total is " + total);
 	}
 
 }
