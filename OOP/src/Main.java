cf0aa1a07e5114c1f5ee0769e195faa822b7a68e
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-11 22:29:30 -0400

    constructors

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -12,12 +12,16 @@ public class Main {
 		// car.stop();
 		// System.out.println(car.isRunning);
 		
+		// Constructors
 		Student student1 = new Student("Sponge", 30, 2.2);
 		Student student2 = new Student("Pat", 34, 2.9);
+		Student student3 = new Student("Abbu Turab", 28, 4.0);
 		
 		System.out.println(student1.name);
 		System.out.println(student2.name);
+		System.out.println(student3.name + " has GPA of " + student3.gpa);
 		
+		student1.study();
 		
 	}
 
--- OOP/src/Student.java
+++ OOP/src/Student.java
@@ -11,4 +11,8 @@ public class Student {
 		this.age = age;
 		this.gpa = gpa;
 	}
+	
+	void study() {
+		System.out.println(this.name + " is studying");
+	}
 }
