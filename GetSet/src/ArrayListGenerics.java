6e630b2df21024375c8f8cadad091767a053f854
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-16 21:49:10 -0400

    commented arrays

--- GetSet/src/ArrayListGenerics.java
+++ GetSet/src/ArrayListGenerics.java
@@ -1,21 +1,21 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class ArrayListGenerics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ArrayList<String> topics = new ArrayList<>();
+	// 	ArrayList<String> topics = new ArrayList<>();
 		
-		topics.add("Java");
-		topics.add("Spring Boot");
-		topics.add("AWS");
+	// 	topics.add("Java");
+	// 	topics.add("Spring Boot");
+	// 	topics.add("AWS");
 		
-		System.out.println("Topics to learn: ");
+	// 	System.out.println("Topics to learn: ");
 		
-		for(String topic : topics) {
-			System.out.println(topic);
-		}
-	}
+	// 	for(String topic : topics) {
+	// 		System.out.println(topic);
+	// 	}
+	// }
 
 }
