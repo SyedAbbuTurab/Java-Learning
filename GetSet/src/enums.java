9481e7286f94ed7d9d4c9b50eabe542d24592869
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-16 23:17:51 -0400

    ENUMS: Practice

--- GetSet/src/enums.java
+++ GetSet/src/enums.java
@@ -4,28 +4,35 @@ public class enums {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Day day = Day.SUNDAY;
+		Day day = Day.FRIDAY;
 			
 //		System.out.println(day);
 //		System.out.println(day.getDatNum());
 		
+//		switch (day) {
+//		case MONDAY, TUESDAY, WEDNESDAY, THURSDAY: {
+//			System.out.println("It's a week day!");
+//			break;
+//		}
+//		case SUNDAY: {
+//			System.out.println("It's a Holiday!");
+//			break;
+//		}
+//		case SATURDAY: {
+//			System.out.println("It's a weekend!");
+//			break;
+//			
+//		}
+//		default:
+//			System.out.println("These day doesn't exist! :(");
+//			break;
+//		}
 		switch (day) {
-		case MONDAY, TUESDAY, WEDNESDAY, THURSDAY: {
-			System.out.println("It's a week day!");
-			break;
-		}
-		case SUNDAY: {
-			System.out.println("It's a Holiday!");
-			break;
-		}
-		case SATURDAY: {
-			System.out.println("It's a weekend!");
-			break;
-			
-		}
-		default:
-			System.out.println("These day doesn't exist! :(");
-			break;
+		case MONDAY, TUESDAY, WEDNESDAY, THURSDAY -> System.out.println("It's a week day!");
+		case SATURDAY -> System.out.println("It's a Weekend!");
+		case SUNDAY -> System.out.println("It's a Holiday");
+		default -> System.out.println("The day does'nt exist");
+		
 		}
 		
 	}
