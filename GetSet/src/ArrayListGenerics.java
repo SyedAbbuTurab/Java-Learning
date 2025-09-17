8cde6590c8af3c936a9369ec8d094733de7f978f
Abbu Turab Syed <syedabbuturab786@gmail.com>
2025-09-16 21:18:12 -0400

    ArrayList: Basic array list created

---
+++ GetSet/src/ArrayListGenerics.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ArrayListGenerics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> topics = new ArrayList<>();
+		
+		topics.add("Java");
+		topics.add("Spring Boot");
+		topics.add("AWS");
+		
+		System.out.println("Topics to learn: ");
+		
+		for(String topic : topics) {
+			System.out.println(topic);
+		}
+	}
+
+}
